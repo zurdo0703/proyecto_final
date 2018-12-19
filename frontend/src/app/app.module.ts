07c084f64280e2d13e263bed1ad5438e7bb835cf
Javier Rojas <javierrojas6@gmail.com>
2018-12-18 23:29:32 -0500

    algunas rutas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { UserProxy } from './../helpers/proxy/user.proxy';
 import { Proxy } from './../helpers/proxy/proxy';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,38 +9,38 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RegisterComponent } from './user/register/register.component';
-import { PostComponent } from './post/post/post.component';
-import { LikeComponent } from './like/like/like.component';
-import { CommentComponent } from './comment/comment/comment.component';
-import { FileComponent } from './file/file/file.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { FileComponent } from './file/file/file.component';
+import { CommentComponent } from './post/comment/comment/comment.component';
+
+import { HomePageModule } from './pages/home/home.module';
+import { RegisterPageModule } from './pages/user/register/register.module';
+import { LoginPageModule } from './pages/user/login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    PostComponent,
-    LikeComponent,
     CommentComponent,
-    FileComponent],
-
+    FileComponent,
+  ],
   entryComponents: [],
-
   imports: [
     BrowserModule,
     HttpClientModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HomePageModule,
+    RegisterPageModule,
+    LoginPageModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
     Proxy,
     HttpClient,
-    UserProxy,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- frontend/src/app/post/post/post.component.ts
+++ frontend/src/app/post/post/post.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
+
 export class PostComponent implements OnInit {
 
   constructor() { }
