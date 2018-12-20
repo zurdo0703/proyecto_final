1a759a881bc26d40b8ff4aa1024ec77078e62f8a
Javier Rojas <javierrojas6@gmail.com>
2018-12-19 19:27:41 -0500

    post FAB

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+  constructor(
+    private navCtrl: NavController,
+  ) { }
+  postAction() {
+    this.navCtrl.navigateForward('postForm');
+  }
 }
 
