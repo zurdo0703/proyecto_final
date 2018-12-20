8632ad50ab4b7978c75b3c274d00a97da4c85954
zurdo0703 <edwinrios@hotmail.com>
2018-12-19 20:13:09 -0500

    file

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +10,33 @@ import { NavController } from '@ionic/angular';
 export class HomePage {
   constructor(
     private navCtrl: NavController,
+    private loadingCtrl: LoadingController,
+    // private toastCtrl: ToastController,
+    // private proxy: Proxy
   ) { }
   postAction() {
     this.navCtrl.navigateForward('postForm');
   }
+  
+  async createPost(event) {
+    console.log(event);
+    const params = {
+      // file: event.file[0].value,
+      comment: event.target[1].value
+      
+    };
+
+    // const loader = await this.loadingCtrl.create({
+    //   message: "Creando Post",
+    //   duration: 3000
+    // });
+    // loader.present();
+
+    // this.proxy.user.save(null, params)
+    //   .then((result: { status: boolean, content: any }) => {
+    //     loader.dismiss();
+
+  }
+
 }
 
